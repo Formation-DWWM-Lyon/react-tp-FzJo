688d453180c7436f89f8af127dcac2adce133d51
JoFz <43348746+FzJo@users.noreply.github.com>
2020-03-05 21:45:32 +0100

    Add route to profil

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import DataContainer from './containers/DataContainer';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Profil from './components/Profil';
 
 const App = () =>
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={DataContainer} />
+      <Route exact path="/profil" component={Profil} />
     </Switch>
   </BrowserRouter>
   ;
