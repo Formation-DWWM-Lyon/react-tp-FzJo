76cf6fb04f213a7881082ae926563604f6b43acc
JoFz <43348746+FzJo@users.noreply.github.com>
2020-03-05 21:14:05 +0100

    redefine Profil component

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import { Card, ListGroup, Button } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 
-const Profil = ({}) => (
+const Profil = ({ age, city, country, email, login, picture, registered }) => (
   <Card>
-    <Card.Header as="h2">{location}</Card.Header>
+    <Card.Header as="h3">{login.username}</Card.Header>
     <Card.Body>
-      <Card.Subtitle>Profile</Card.Subtitle>
+      <Card.Subtitle>Profil</Card.Subtitle>
+      <Image src={picture.large} roundedCircle />
       <Button variant="primary">Go somewhere</Button>
     </Card.Body>
     <ListGroup variant="info">
-      <ListGroup.Item>Gender: {gender}</ListGroup.Item>
-      <ListGroup.Item>Email: {email}</ListGroup.Item>
+      <ListGroup.Item>Age : {age}</ListGroup.Item>
+      <ListGroup.Item>City : {city}</ListGroup.Item>
+      <ListGroup.Item>Country : {country}</ListGroup.Item>
+      <ListGroup.Item>Email : {email}</ListGroup.Item>
     </ListGroup>
     <Card.Footer>
       <small className="text-muted">
